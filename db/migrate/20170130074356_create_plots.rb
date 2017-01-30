3be3a545fecab547c6914b78986b9b118be36d15
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-01-30 10:44:28 +0300

    Add Plot

---
+++ db/migrate/20170130074356_create_plots.rb
@@ -0,0 +1,13 @@
+class CreatePlots < ActiveRecord::Migration[5.0]
+  def change
+    create_table :plots do |t|
+      t.string :number
+      t.integer :space
+      t.string :cadastre
+      t.string :ukrgosact
+      t.references :member, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
