fef6bd001dfeba052767d9e9dd067b07eb084de3
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-01-30 19:40:49 +0300

    Improve error handling when plot doesn't exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  add_flash_types :danger, :warning, :info, :success
 
   def set_locale
     I18n.locale = :ru
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,6 +4,9 @@ class PaymentsController < ApplicationController
 
   def new
     @payment = Payment.new(member: Member.owner_of(plot_number))
+  rescue ActiveRecord::RecordNotFound => e
+    raise e unless e.model == "Plot"
+    redirect_to :payments, danger: t('payments.no_such_plot')
   end
 
   def plot_number
