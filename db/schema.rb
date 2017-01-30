f7e0d5113e624c4714a8be473fb38e4c1917c7ab
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-01-30 11:00:05 +0300

    Add Member#status

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170130075415) do
     t.string   "fio"
     t.string   "address"
     t.string   "phone"
+    t.string   "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
