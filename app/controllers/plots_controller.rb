fb3855e23e35c095b55b26948d752bd41496c6a5
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-02-01 00:14:19 +0300

    Refactor Plot loading

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,11 +1,12 @@
 class PlotsController < ApplicationController
+  before_action :load_plot, only: %w(edit update destroy)
+
   def index
     self.page_title = t 'plots.index.title'
     @plots = Plot.order(:number)
   end
 
   def edit
-    @plot = Plot.find(params[:id])
     self.page_title = t 'plots.edit.title', number: @plot.number
   end
 
@@ -21,16 +22,18 @@ class PlotsController < ApplicationController
   end
 
   def update
-    @plot = Plot.find(params[:id])
     self.page_title = t 'plots.edit.title', number: @plot.number
     @plot.update(plot_params)
     respond_with @plot
   end
 
   def destroy
-    plot = Plot.find(params[:id])
-    plot.destroy
-    respond_with plot
+    @plot.destroy
+    respond_with @plot
+  end
+
+  def load_plot
+    @plot = Plot.find(params[:id])
   end
 
   def plot_params
