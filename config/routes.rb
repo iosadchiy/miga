56172034b9e0ac0cac402de2fc622d72f811ad34
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-02-01 21:16:59 +0300

    Lessen number of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'payments#index'
-  resources :members
+  resources :members, except: :show
   resources :payments
-  resources :plots
-  resources :dues
-  resource :setting
+  resources :plots, except: :show
+  resources :dues, except: :show
+  resource :setting, only: [:edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
