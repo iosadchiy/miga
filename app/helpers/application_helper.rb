82e179f47f945ca8288c581633cf4a197ead17ef
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-02-02 17:07:14 +0300

    Render Register#kind as proper select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # @param model class - The model class (e.g. User)
+  # @param enum symbol - The enum defined on `model` (e.g. :status)
+  # Returns: [["Активен", :active], ["Исключен", :deleted]]
+  def options_for_enum(model, enum)
+    model.send(enum.to_s.pluralize).keys.map { |key|
+      [model.human_attribute_name("#{enum}.#{key}"), key]
+    }
+  end
 end
