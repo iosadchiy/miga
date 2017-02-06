d80275a6cc10a4c7295192703137a22db3751157
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-02-06 13:17:15 +0300

    Refactor PaymentsController

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
 
   class PlotNotValidError < RuntimeError; end
+  before_action :load_member, only: [:new, :create]
 
   def index
     self.page_title = t('payments.title')
@@ -8,12 +9,19 @@ class PaymentsController < ApplicationController
 
   def new
     self.page_title = t('payments.new.title')
+    @payment = Payment.new(member: @member.__getobj__)
+  end
+
+  def create
+    self.page_title = t('payments.new.title')
+  end
+
+  def load_member
     plot = Plot.find_by(number: plot_number) or
       raise PlotNotValidError.new t('payments.no_such_plot')
     member = plot.member or
       raise PlotNotValidError.new t('payments.no_member')
     @member = MemberPresenter.new(member)
-    @payment = Payment.new(member: member)
   rescue PlotNotValidError => e
     redirect_back fallback_location: :payments, danger: e.message
   end
