813dd45a5f79442d1499f06fb3c1c05dd4a9b0d3
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-02-16 22:10:42 +0300

    Add new/edit for dues

--- app/controllers/dues_controller.rb
+++ app/controllers/dues_controller.rb
@@ -5,4 +5,31 @@ class DuesController < ApplicationController
     @target_dues = Due.target
     @membership_dues = Due.membership
   end
+
+  def new
+    self.page_title = t 'dues.new.title'
+    @due = Due.new(kind: params[:kind])
+  end
+
+  def create
+    self.page_title = t 'dues.new.title'
+    @due = Due.create(due_params)
+    respond_with @due
+  end
+
+  def edit
+    self.page_title = t 'dues.edit.title'
+    @due = Due.find(params[:id])
+  end
+
+  def update
+    self.page_title = t 'dues.edit.title'
+    @due = Due.find(params[:id])
+    @due.update(due_params)
+    respond_with @due
+  end
+
+  def due_params
+    params.require(:due).permit(:kind, :purpose, :unit, :price)
+  end
 end
