b11dcc8b4debc33ac164d7098dc2c71bf01cd053
Ivan Osadchii <ivan.osadchii@gmail.com>
2017-02-22 20:02:00 +0300

    Set timezone

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module MigaBe
 
     config.autoload_paths += %W(#{config.root}/app/models/transactions)
 
+    config.time_zone = 'Moscow'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
